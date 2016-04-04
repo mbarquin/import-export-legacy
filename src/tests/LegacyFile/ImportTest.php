6b9b6a7643cfc9be2b36f27915510e0860e66c4a
mbarquin <moises.barquin@gmail.com>
2016-04-04 07:19:14 +0200

    Tests on import object

--- src/tests/LegacyFile/ImportTest.php
+++ src/tests/LegacyFile/ImportTest.php
@@ -15,4 +15,27 @@ class ImportTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf('mbarquin\LegacyFile\Import', $oImport);
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testBadPathConstruct()
+    {
+        $defArray = array (
+            'name'    => 20,
+            'surname' => 20,
+            'phone'   => 10
+        );
+        $oImport = new Import('./files/contactsFileNotExists.csv', $defArray);
+    }
+    
+    public function testBadDefinitionConstruct()
+    {
+        $defArray = array (
+            'name'    => 20,
+            'surname' => 20,
+            'phone'   => 10
+        );
+        $oImport = new Import('./files/contacts.csv', 'Test');
+    }
+    
 }
